14323dd3ab05a7fae81942864ccd226c43bd4229
Joe Hines <jth95@drexel.edu>
2018-07-08 23:29:05 -0400

    New bar using gohufont

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,7 +10,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 #polybar example &
 
-for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar example -c ~/.config/polybar/config & done
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar base -c ~/.config/polybar/config & done
 #feh --bg-scale ~/.config/wall.png
 
 #polybar example -c ~/.config/polybar/config
